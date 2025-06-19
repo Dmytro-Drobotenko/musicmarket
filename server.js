34cd111be7151b879b0135c5f91d7e898da81046
Usuario <drobotenkodmitro06@gmail.com>
2025-06-19 12:35:10 +0200

    Оновлення файлів

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ async function connectToMongo() {
 }
 connectToMongo();
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/top-products", async (req, res) => {
   try {
     const orderItems = db.collection("orderitem");
